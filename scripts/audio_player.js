4bd1b95fa9949490131bb2f4177c5296849e0107
Whiteline <48377708+xWhiteline@users.noreply.github.com>
2020-12-30 16:41:21 +0200

    updated the page.

---
+++ scripts/audio_player.js
@@ -0,0 +1,14 @@
+var cpo = []; 
+cpo["_object"] ="cp_widget_8facceac-bdf7-4c64-8ddf-b3524e4d8dc5";
+cpo["_fid"] = "AQFAEwOgeAV7";
+
+var _cpmp = _cpmp || [];
+_cpmp.push(cpo);
+
+(function() { var cp = document.createElement("script"); 
+cp.type = "text/javascript";
+cp.async = true; 
+cp.src = "//www.cincopa.com/media-platform/runtime/libasync.js";
+
+var c = document.getElementsByTagName("script")[0];
+c.parentNode.insertBefore(cp, c); })();
