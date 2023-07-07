e78273788113aec3639f223ae5159763e42f9624
Morar Andrei - Alexandru <whiteex@icloud.com>
2023-07-07 14:46:04 +0300

    July 7th, 2023
    - Trying to get the Google Analytics to work smh

--- scripts/analytics.js
+++
@@ -1,9 +0,0 @@
-window.onload = Startup();
-
-function Startup(){
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-    
-    gtag('config', 'G-XYPSQRD8Q5');
-}
