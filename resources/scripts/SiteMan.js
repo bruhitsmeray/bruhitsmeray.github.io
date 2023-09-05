ca10bb713f8a4a74772da3f63202ce09f85922b4
Morar Andrei - Alexandru <48377708+itsme-ray@users.noreply.github.com>
2023-09-05 14:46:38 +0300

    Remove hard-coded "highlight" class

--- resources/scripts/SiteMan.js
+++ resources/scripts/SiteMan.js
@@ -1,7 +1,17 @@
+window.onload = assignBoxClass();
+
 for(const box of document.querySelectorAll('.box')){
     box.onclick = () => {
         if(box.hasAttribute('href')){
             window.open(box.getAttribute("href"));
+        }
+    }
+}
+
+function assignBoxClass() {
+    for(const box of document.querySelectorAll('.box')){
+        if(box.hasAttribute('href')){
+            box.classList.add('highlight');
         }
     }
 }
