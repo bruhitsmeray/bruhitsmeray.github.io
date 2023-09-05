64273303c0cd01385dfacfffe6f0da4886362a06
Morar Andrei - Alexandru <48377708+itsme-ray@users.noreply.github.com>
2023-09-05 14:51:45 +0300

    Update SiteMan.js

--- resources/scripts/SiteMan.js
+++ resources/scripts/SiteMan.js
@@ -1,6 +1,8 @@
+const boxes = document.querySelectorAll('.box');
+
 window.onload = assignBoxClass();
 
-for(const box of document.querySelectorAll('.box')){
+for(const box of boxes){
     box.onclick = () => {
         if(box.hasAttribute('href')){
             window.open(box.getAttribute("href"));
@@ -9,7 +11,7 @@ for(const box of document.querySelectorAll('.box')){
 }
 
 function assignBoxClass() {
-    for(const box of document.querySelectorAll('.box')){
+    for(const box of boxes){
         if(box.hasAttribute('href')){
             box.classList.add('highlight');
         }
